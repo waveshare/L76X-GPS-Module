84961f1e95ba5b3d35f0f881524705bb3d6e3664
digiwhale <digiwhale.humphrey@gmail.com>
2022-03-18 00:46:28 -0400

    1

--- python/main.py
+++ python/main.py
@@ -6,14 +6,14 @@ import traceback
 try:
     x=L76X.L76X()
     x.L76X_Set_Baudrate(9600)
-    # x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
+    x.L76X_Send_Command(x.SET_NMEA_BAUDRATE_115200)
     time.sleep(2)
     x.L76X_Set_Baudrate(115200)
 
-    # x.L76X_Send_Command(x.SET_POS_FIX_400MS)
+    x.L76X_Send_Command(x.SET_POS_FIX_400MS)
 
     #Set output message
-    # x.L76X_Send_Command(x.SET_NMEA_OUTPUT)
+    x.L76X_Send_Command(x.SET_NMEA_OUTPUT)
 
     x.L76X_Exit_BackupMode()
     while(1):
